297a8ac0be1bc1e69ff3ba477d2a6120586bcf12
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2020-11-09 16:02:01 +0100

    Classe Joueur

--- joueur.py
+++ joueur.py
@@ -1,2 +1,19 @@
-nom_joueur = input("Capitaine, comment-vous appelez vous ?").upper()
+
+# Classe Joueur
+class Joueur:
+    """
+    :param nom: le nom de l'utilisateur
+    """
+    def __init__(self, nom):
+        self.username = nom
+        self.function = "Capitaine"
+
+    def getNom(self):
+        return self.username
+
+    def getFunction(self):
+        return self.function
+
+    def __str__(self):
+        return "Le nouveau {} est {}, tout le monde à ses ordres !".format(self.function, self.username)
 
