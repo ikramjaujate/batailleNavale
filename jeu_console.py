7499c6c0d9e93120e8839eb88dc8aa648029ca56
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2020-12-07 13:32:12 +0100

    Version console : Finale

--- jeu_console.py
+++ jeu_console.py
@@ -3,7 +3,6 @@ from batailleNavale.joueur import Joueur
 from batailleNavale.ocean import Ocean
 from batailleNavale.bateau import *
 from batailleNavale.clear import clear
-from batailleNavale.tir import Tir
 
 hauteur = 0
 largeur = 0
@@ -55,13 +54,14 @@ ma_grille = ocean.grille()
 
 grille_ennemie = ocean.grille()
 grille_tirs = ocean.grille()
+
 coord_bateau_utilisateur = []
 coord_bateau_ordi = []
 
 
 class Bateau:
 
-    def print_grille(self, gri):
+    def print_grille(self, gri : list):
         for ligne in gri:
             print("  ".join(ligne))
 
@@ -235,6 +235,66 @@ utilisateur_place = b.utilisateur_placer_bateaux(ma_grille, total_bateau)
 ordi_place = b.ordinateur_placer_bateaux(grille_tirs, total_bateau)
 
 
+class Tir:
+
+    def tir(self, grille : list, x: int, y: int) -> str:
+        etat = ""
+        if grille[x][y] == ".":
+            etat += "rate"
+            return etat
+        elif grille[x][y] == "*" or grille[x][y] == "X":
+            etat += "toucheAvant"
+            return etat
+        else:
+            etat += "touche"
+            return etat
+
+    def utilisateur_tir(self, grille: list, tableau : list, grille_affiche : list, points: int):
+        while True:
+            print("Capitaine, ou voulez vous tiré ?")
+            x, y = b.get_coordonnes()
+            tir = self.tir(grille, x, y)
+            if tir == "rate":
+                print("Capitaine, vous avez rate le tir")
+                grille[x][y] = "*"
+                grille_affiche[x][y] = "*"
+            elif tir == "toucheAvant":
+                print("Capitaine, vous avez déjà touché")
+            elif tir == "touche":
+                points += 1
+                print("Bravo capitaine, vous avez touché le bateau")
+                grille[x][y] = "X"
+                grille_affiche[x][y] = "X"
+                if tir == "touche":
+                    grille[x][y] = 'X'
+                    for liste in tableau:
+                        for element in liste:
+                            if (x, y) == element:
+                                liste.remove((x, y))
+
+            if tir != "toucheAvant":
+                return grille
+
+    def tir_ordinateur(self, grille: list, tableau : list, points: int):
+        """
+        générer des coordonnées aléatoires pour que l'ordinateur réalise les mouvements
+        """
+        while True:
+            x = random.randint(1, hauteur)
+            y = random.randint(1, hauteur)
+            tir = self.tir(grille, x, y)
+            if tir == "touche":
+                points += 1
+                print("NOOOOON !!! MOUSSAILLON, ILS ONT TOUCHÉ UN DE NOS BATEAUX ! ")
+                grille[x][y] = 'X'
+                for liste in tableau:
+                    for element in liste:
+                        if (x, y) == element:
+                            liste.remove((x, y))
+            elif tir == "rate":
+                print("HIHI, ils ont touché la balaine ")
+                grille[x][y] = "*"
+            return grille
 
 
 # Encore un tours
