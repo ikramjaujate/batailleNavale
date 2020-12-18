2415a5df95a65abd1d3066acf36478e3de54d654
ikramjaujate <ikramhamdi.hamdi3@gmail.com>
2020-12-18 18:18:58 +0100

    Fichiers

--- print_text.py
+++ print_text.py
@@ -1,4 +1,4 @@
-def print_grille(grille: list):
+def print_grille(grille: list) -> str:
     """
     Function qui permet de afficher la grille en console
     :param grille: la grille qui est utilisé
@@ -9,37 +9,42 @@ def print_grille(grille: list):
     for ligne in grille:
         print("  ".join(ligne))
 
-def plouf(grille: list):
+
+def plouf(grille: list) -> str:
     print("Capitaine, vous avez rate le tir")
     print("------------------------GRILLE ENNEMI ---------------------------")
     print_grille(grille)
 
 
-def boum(grille: list):
+def boum(grille: list) -> str:
     print("Bravo capitaine, vous avez touché le bateau")
     print("------------------------GRILLE ENNEMI ---------------------------")
     print_grille(grille)
 
 
-def ploufOrdi(grille: list):
+def ploufOrdi(grille: list) -> str:
     print("HIHI, ils ont touché la balaine ")
     print("------------------------MA GRILLE ---------------------------")
     print_grille(grille)
 
 
-def boumOrdi(grille: list):
+def boumOrdi(grille: list) -> str:
     print("NOOOOON !!! MOUSSAILLON, ILS ONT TOUCHÉ UN DE NOS BATEAUX ! ")
     print("------------------------MA GRILLE ---------------------------")
     print_grille(grille)
 
-def joueur_gagne():
-    print("Jack Sparrow serait très fière de vous capitaine, ")
 
-def ordi_gagne():
-    print("Capitaine, vous nous avez deçu....")
+def joueur_gagne(points) -> str:
+    print(" Jack Sparrow serait très fière de vous capitaine, vous avez gagné avec " + points + " points")
 
-def egalite():
-    print("C'était une très bonne partie mon capitaine, vous êtes à égalité ")
 
-def chiffre_pas_valable():
+def ordi_gagne(points) -> str:
+    print("Capitaine , vous nous avez deçu....vous avez perdu avec : " + points + " points")
+
+
+def egalite(points) -> str:
+    print("C'était une très bonne partie mon capitaine, vous êtes à égalité avec " + points + " points chacun")
+
+
+def chiffre_pas_valable() -> str:
     print("Chiffre pas valable")
